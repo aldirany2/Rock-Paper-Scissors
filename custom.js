e65911f679e4a8194fb50f5355abc92fb362bfb4
Mohamed Hassan <aldirany2016@gmail.com>
2025-08-27 18:15:05 +0300

    first update for my game

--- custom.js
+++ custom.js
@@ -1,1 +1,18 @@
-console.log("hii, i'm working well")
+const choose = ["rock", "paper", "scissors"]
+function getComputerChoice() {
+    const randomChoose = Math.floor(Math.random() * choose.length);
+    const theChoice = choose[randomChoose];
+    const compResult = document.querySelector("#compResult").innerHTML = theChoice;
+
+}
+
+getComputerChoice();
+
+
+const rock = document.querySelector(".rock").addEventListener("click", getHumanChoice);
+const paper = document.querySelector(".paper").addEventListener("click", getHumanChoice);
+const scissors = document.querySelector(".scissors").addEventListener("click", getHumanChoice);
+function getHumanChoice() {
+    const result = document.querySelector("#result").innerHTML = this.value;
+
+}
